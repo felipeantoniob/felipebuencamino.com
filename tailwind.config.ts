1af0d12701fd1fb77ff04062636b2bdd0c64df26
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-09-09 21:56:24 +0800

    Add sen font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 import formsPlugin from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Sen", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [formsPlugin],
 } satisfies Config;
