a7adbfbf72c06996ce1c2598a9661ff9b006357f
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-09-09 21:53:00 +0800

    Add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,24 @@
+import { fileURLToPath } from "url";
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type { PrettierConfig | TailwindConfig } */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  tailwindConfig: fileURLToPath(
+    new URL("./tailwind.config.ts", import.meta.url),
+  ),
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
+
+
