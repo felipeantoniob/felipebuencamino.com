840733940ef39c9fa62663cb8ca6e04838ac40bd
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-09-19 19:46:58 +0800

    Add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), icon()],
 });
