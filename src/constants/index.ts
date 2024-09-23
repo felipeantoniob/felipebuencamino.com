b9eda19b4309724859b4c36fb4ba63ba5b0249c3
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-09-23 19:57:43 +0800

    Add ProjectsSection

---
+++ src/constants/index.ts
@@ -0,0 +1,103 @@
+export const PROJECTS = [
+  {
+    title: "Security Bank App",
+    description:
+      "A mobile app designed for intuitive online banking, featuring secure transactions, real-time account management, and a user-friendly interface for seamless financial oversight.",
+    tags: ["react", "javascript"],
+    codeUrl: null,
+    siteUrl: "https://mexplay.mx/",
+    id: 0,
+    technologies: [
+      { name: "React Native", url: "https://reactjs.org/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "Redux", url: "https://redux.js.org/" },
+      { name: "Firebase", url: "https://firebase.google.com/" },
+      { name: "React Navigation", url: "https://reactnavigation.org/" },
+      { name: "Sass", url: "https://sass-lang.com/" },
+    ],
+  },
+  {
+    title: "Mexplay",
+    description:
+      "A web and mobile app that offers a dynamic casino experience with a wide range of games, betting options, and exciting prizes. ",
+    tags: ["react", "javascript"],
+    codeUrl: null,
+    siteUrl: "https://mexplay.mx/",
+    id: 0,
+    technologies: [
+      { name: "React", url: "https://react.dev/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "Redux", url: "https://redux.js.org/" },
+      { name: "styled-components", url: "https://styled-components.com/" },
+      { name: "i18next", url: "https://www.i18next.com/" },
+    ],
+  },
+  {
+    title: "Spotify Replay",
+    description:
+      "A web app where users can view their top artists, tracks, and genres on Spotify. They can also generate playlists based on their personalized data to share with friends.",
+    tags: ["react", "javascript"],
+    codeUrl: "https://github.com/felipeantoniob/spotify-replay#readme",
+    siteUrl: "https://spotify-replay.vercel.app/",
+    id: 1,
+    technologies: [
+      { name: "Next.js", url: "https://nextjs.org/" },
+      { name: "React", url: "https://react.dev/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "tRPC", url: "https://trpc.io/" },
+      { name: "Tailwind", url: "https://tailwindcss.com/" },
+      { name: "NextAuth", url: "https://next-auth.js.org/" },
+      { name: "Turborepo", url: "https://turbo.build/" },
+      {
+        name: "zustand",
+        url: "https://docs.pmnd.rs/zustand/getting-started/introduction",
+      },
+    ],
+  },
+  {
+    title: "2ner",
+    description:
+      "An online tuner that makes tuning instruments quick and accurate, it offers real-time tuning through a straightforward and intuitive interface.",
+    tags: ["react", "javascript"],
+    codeUrl: "https://github.com/felipeantoniob/todo-list#readme",
+    siteUrl: "https://next-todo-app-two.vercel.app",
+    id: 4,
+    technologies: [
+      { name: "Vite", url: "https://vitejs.dev/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "Tailwind", url: "https://tailwindcss.com/" },
+      { name: "React", url: "https://react.dev/" },
+    ],
+  },
+  {
+    title: "Cinehub",
+    description:
+      "A movie database app that allows users to search for movies and view details such as the plot summary, genres, and actors and uses the TMDB API to retrieve data on the movies.",
+    tags: ["react", "javascript"],
+    codeUrl: "https://github.com/felipeantoniob/cinehub#readme",
+    siteUrl: "https://cinehub.vercel.app",
+    id: 2,
+    technologies: [
+      { name: "Next.js", url: "https://nextjs.org/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "Tailwind", url: "https://tailwindcss.com/" },
+      { name: "tRPC", url: "https://trpc.io/" },
+      { name: "Prisma", url: "https://www.prisma.io/" },
+    ],
+  },
+  {
+    title: "TS-808",
+    description:
+      "An interactive drum machine website where users can play 16 drum pads with their mouse or keyboard. The drum samples were sampled from a Roland TR-808 drum machine.",
+    tags: ["react", "javascript"],
+    codeUrl: "https://github.com/felipeantoniob/TS-808#readme",
+    siteUrl: "https://ts-808.vercel.app/",
+    id: 3,
+    technologies: [
+      { name: "Vite", url: "https://vitejs.dev/" },
+      { name: "Vitest", url: "https://vitest.dev/" },
+      { name: "Typescript", url: "https://www.typescriptlang.org/" },
+      { name: "Tailwind", url: "https://tailwindcss.com/" },
+    ],
+  },
+] as const;
