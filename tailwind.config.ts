cce00ecd44b87e7c5219f300f6fd5a16a50323b0
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-12-17 21:52:54 +0800

    Redesign

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,51 @@ export default {
       fontFamily: {
         sans: ["Sen", ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        'container-shadow': '.15rem .15rem 0 0 #232b34, .3rem .3rem 0 0 #232b34, .45rem .45rem 0 0 #232b34, .6rem .6rem 0 0 #232b34',
+      },
+      animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+      },
+      keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+      }
     },
   },
   plugins: [
