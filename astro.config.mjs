7592ed8e2f9f92a5dd91327847b603fe4333c97f
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-12-18 22:32:21 +0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  site: "https://felipe-buencamino.onrender.com",
+  output: "static",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "load",
+  },
+  integrations: [react(), tailwind(), sitemap(), icon()],
 });
