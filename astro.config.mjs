7158491bd350b8d4e1ea791fc0a3d1644de485c7
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2025-01-14 22:03:25 +0800

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://felipe-buencamino.onrender.com",
+  site: "https://www.felipebuencamino.com",
   output: "static",
   prefetch: {
     prefetchAll: true,
