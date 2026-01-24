54bfa8ed1e623b0ff005acf6e7194ea063bd70fc
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2026-01-24 20:55:55 +0800

    chore: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,31 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.felipebuencamino.com",
   output: "static",
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "load",
   },
-  integrations: [react(), tailwind(), sitemap(), icon(), partytown()],
+
+  integrations: [react(),  sitemap(), icon(), partytown()],
+
   server: {
     headers: {
       "Content-Security-Policy":
         "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; block-all-mixed-content; upgrade-insecure-requests;",
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
